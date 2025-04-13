77932c1b4197ad41b9349f65009ef445a3387063
hitrai007 <hitrai007@gmail.com>
2025-04-13 23:13:55 +0530

    fix: add missing dependencies and update web3modal imports

--- src/components/WalletProvider.tsx
+++ src/components/WalletProvider.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import React from 'react'
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { baseSepolia } from 'wagmi/chains'
