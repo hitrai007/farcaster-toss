dd5f2b546acb3a1c5961436a524895da19ac5e31
hitrai007 <hitrai007@gmail.com>
2025-04-25 01:08:22 +0530

    Refactor: Use standard JSON ABI format for contracts

--- src/contracts/constants.ts
+++ src/contracts/constants.ts
@@ -1,20 +1,116 @@
-// Contract ABI
+// Contract ABI - Standard JSON Format
 export const COIN_TOSS_GAME_ABI = [
-  "function startGame(bool choice, address token) external",
-  "function joinGame(bool choice, address token) external",
-  "function getGameState() external view returns (address,address,bool,bool,address,bool,address)",
-  "event GameStarted(address indexed player1, bool choice, address token)",
-  "event GameJoined(address indexed player2, bool choice)",
-  "event GameCompleted(address indexed winner, uint256 amount)",
-  "event RandomnessRequested(uint256 requestId)",
-  "event RandomnessFulfilled(uint256 requestId, uint256 randomWord)"
+  {
+    "type": "function",
+    "name": "startGame",
+    "inputs": [
+      { "name": "choice", "type": "bool", "internalType": "bool" },
+      { "name": "token", "type": "address", "internalType": "address" }
+    ],
+    "outputs": [],
+    "stateMutability": "external"
+  },
+  {
+    "type": "function",
+    "name": "joinGame",
+    "inputs": [
+      { "name": "choice", "type": "bool", "internalType": "bool" },
+      { "name": "token", "type": "address", "internalType": "address" }
+    ],
+    "outputs": [],
+    "stateMutability": "external"
+  },
+  {
+    "type": "function",
+    "name": "getGameState",
+    "inputs": [],
+    "outputs": [
+      { "name": "", "type": "address", "internalType": "address" },
+      { "name": "", "type": "address", "internalType": "address" },
+      { "name": "", "type": "bool", "internalType": "bool" },
+      { "name": "", "type": "bool", "internalType": "bool" },
+      { "name": "", "type": "address", "internalType": "address" },
+      { "name": "", "type": "bool", "internalType": "bool" },
+      { "name": "", "type": "address", "internalType": "address" }
+    ],
+    "stateMutability": "view"
+  },
+  { 
+    "type": "event", 
+    "name": "GameStarted", 
+    "inputs": [
+      { "name": "player1", "type": "address", "indexed": true, "internalType": "address" },
+      { "name": "choice", "type": "bool", "indexed": false, "internalType": "bool" },
+      { "name": "token", "type": "address", "indexed": false, "internalType": "address" }
+    ],
+    "anonymous": false
+  },
+  { 
+    "type": "event", 
+    "name": "GameJoined", 
+    "inputs": [
+      { "name": "player2", "type": "address", "indexed": true, "internalType": "address" },
+      { "name": "choice", "type": "bool", "indexed": false, "internalType": "bool" }
+    ],
+    "anonymous": false
+  },
+  { 
+    "type": "event", 
+    "name": "GameCompleted", 
+    "inputs": [
+      { "name": "winner", "type": "address", "indexed": true, "internalType": "address" },
+      { "name": "amount", "type": "uint256", "indexed": false, "internalType": "uint256" }
+    ],
+    "anonymous": false
+  },
+  { 
+    "type": "event", 
+    "name": "RandomnessRequested", 
+    "inputs": [
+      { "name": "requestId", "type": "uint256", "indexed": false, "internalType": "uint256" }
+    ],
+    "anonymous": false
+  },
+  { 
+    "type": "event", 
+    "name": "RandomnessFulfilled", 
+    "inputs": [
+      { "name": "requestId", "type": "uint256", "indexed": false, "internalType": "uint256" },
+      { "name": "randomWord", "type": "uint256", "indexed": false, "internalType": "uint256" }
+    ],
+    "anonymous": false
+  }
 ] as const;
 
-// ERC20 ABI
+// ERC20 ABI - Standard JSON Format
 export const ERC20_ABI = [
-  "function approve(address spender, uint256 amount) external returns (bool)",
-  "function allowance(address owner, address spender) external view returns (uint256)",
-  "function balanceOf(address account) external view returns (uint256)"
+  {
+    "type": "function",
+    "name": "approve",
+    "inputs": [
+      { "name": "spender", "type": "address", "internalType": "address" },
+      { "name": "amount", "type": "uint256", "internalType": "uint256" }
+    ],
+    "outputs": [ { "name": "", "type": "bool", "internalType": "bool" } ],
+    "stateMutability": "external"
+  },
+  {
+    "type": "function",
+    "name": "allowance",
+    "inputs": [
+      { "name": "owner", "type": "address", "internalType": "address" },
+      { "name": "spender", "type": "address", "internalType": "address" }
+    ],
+    "outputs": [ { "name": "", "type": "uint256", "internalType": "uint256" } ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "balanceOf",
+    "inputs": [ { "name": "account", "type": "address", "internalType": "address" } ],
+    "outputs": [ { "name": "", "type": "uint256", "internalType": "uint256" } ],
+    "stateMutability": "view"
+  }
 ] as const;
 
 // Contract addresses
