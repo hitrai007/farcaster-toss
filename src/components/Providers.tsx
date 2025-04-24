1ea74540eb785d56e9313cbfcd4df576c896ad60
hitrai007 <hitrai007@gmail.com>
2025-04-24 21:06:52 +0530

    fix: update WalletProvider import in Providers component

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import WalletProvider from './WalletProvider'
+import { WalletProvider } from './WalletProvider'
 import { FarcasterProvider } from './FarcasterProvider'
 import { Toaster } from 'react-hot-toast'
 
